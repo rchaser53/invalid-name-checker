22dc7de406b7783ce5a82217cd8ffe4107ef2aac
yoshizawa.takayuki <yoshizawa.takayuki@linecorp.com>
2019-03-29 17:14:21 +0900

    setup

---
+++ index.js
@@ -0,0 +1,7 @@
+// require the module
+var owasp = require('owasp-password-strength-test');
+ 
+// invoke test() to test the strength of a password
+const result = owasp.test('bundl141sadfa21341s34fasv.js');
+
+console.log(result)
