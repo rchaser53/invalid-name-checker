e3b992d52ddcdb8b2c56428b8669c258f587eda3
rchaser53 <tayoshizawa29@gmail.com>
2019-04-03 23:42:59 +0900

    try to test invaild name checker

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const zxcvbn = require('zxcvbn');
  
 module.exports = (input) => {
-  return zxcvbn(input);
+  return zxcvbn(input).score > 2;
 };
--- test.js
+++ test.js
@@ -1,5 +1,14 @@
+const generateHash = require("random-hash").generateHash;
+const check = require("./index");
+
 describe('hello', () => {
   test('world', async () => {
-    expect(0x01 << ~~(7 / 8)).toEqual(0x01);
+    
+    expect(check(generateHash({ length: 4 }))).toBe(false)
+    expect(check(generateHash({ length: 5 }))).toBe(false)
+    expect(check(generateHash({ length: 6 }))).toBe(false)
+    expect(check(generateHash({ length: 7 }))).toBe(false)
+    expect(check(generateHash({ length: 8 }))).toBe(false)
+    expect(check(generateHash({ length: 9 }))).toBe(true)
   });
 });
